f65f9a6c0772bd9610762a0cb56a748bd7e2d403
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 20:25:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,6 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.select do |kids_name, age|
+    age <= 12
   birthday_kids.each do |kids_name, age| if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
@@ -12,9 +14,9 @@ def happy_birthday(birthday_kids)
 end
 
 
-# my_stocks.select do |_name, data|
-#   data['price'] > 500
-# end.sort_by do |_name, data|
-#   -data['price']
-# end.each do |name, data|
-#   # ... print or whatever
+my_stocks.select do |_name, data|
+  data['price'] > 500
+end.sort_by do |_name, data|
+  -data['price']
+end.each do |name, data|
+  # ... print or whatever
