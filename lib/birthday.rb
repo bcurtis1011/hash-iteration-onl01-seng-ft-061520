c3042484d2bc87915cbbe1b64bc431cc84f7d41a
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-06-28 20:19:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    if age <= 12
+  birthday_kids.each do |kids_name, age| if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
